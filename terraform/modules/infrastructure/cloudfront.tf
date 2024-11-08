bed1078f3b385db4ec3306bba5654fb7dff7ebd1
koki-develop <kou.pg.0131@gmail.com>
2024-11-09 07:59:33 +0900

    Create cloudfront distribution

---
+++ terraform/modules/infrastructure/cloudfront.tf
@@ -0,0 +1,43 @@
+data "aws_cloudfront_origin_request_policy" "cors_s3_origin" {
+  name = "Managed-CORS-S3Origin"
+}
+
+data "aws_cloudfront_cache_policy" "caching_disabled" {
+  name = "Managed-CachingDisabled"
+}
+
+resource "aws_cloudfront_distribution" "frontend" {
+  enabled = true
+
+  origin {
+    origin_id                = module.s3_bucket_frontend.this.id
+    domain_name              = module.s3_bucket_frontend.this.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.frontend.id
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  default_cache_behavior {
+    target_origin_id         = module.s3_bucket_frontend.this.id
+    viewer_protocol_policy   = "redirect-to-https"
+    cached_methods           = ["GET", "HEAD"]
+    allowed_methods          = ["GET", "HEAD"]
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.cors_s3_origin.id
+    cache_policy_id          = data.aws_cloudfront_cache_policy.caching_disabled.id
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "frontend" {
+  name                              = "${var.name}-frontend"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
--- terraform/modules/infrastructure/s3.tf
+++ terraform/modules/infrastructure/s3.tf
@@ -2,3 +2,24 @@ module "s3_bucket_frontend" {
   source = "../common/s3_bucket"
   bucket = "${var.name}-frontend"
 }
+
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = module.s3_bucket_frontend.this.id
+  policy = data.aws_iam_policy_document.s3_bucket_frontend_policy.json
+}
+
+data "aws_iam_policy_document" "s3_bucket_frontend_policy" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${module.s3_bucket_frontend.this.arn}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_cloudfront_distribution.frontend.arn]
+    }
+  }
+}
